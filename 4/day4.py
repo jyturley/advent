8113943b5d2c86a17bf5b4a440f50df3f8c0729a
jyturley <jyturley@gmail.com>
2015-12-06 14:05:30 -0800

    finish day 4

--- 4/day4.py
+++ 4/day4.py
@@ -2,13 +2,24 @@
 import md5
 import time
 
-def find_hash(secret_key):
+def find_hash_5(secret_key):
 	max_duration = time.time() + 5 * 60 #5 minutes
 	so_far = 0
 	while time.time() < max_duration:
 		try_key = secret_key + str(so_far)
 
 		if md5.new(try_key).hexdigest()[:5] == "00000":
+			return so_far
+		else:
+			so_far += 1
+
+def find_hash_6(secret_key):
+	max_duration = time.time() + 5 * 60 #5 minutes
+	so_far = 0
+	while time.time() < max_duration:
+		try_key = secret_key + str(so_far)
+
+		if md5.new(try_key).hexdigest()[:6] == "000000":
 			return so_far
 		else:
 			so_far += 1
@@ -20,11 +31,13 @@ def test():
 def partone():
 	hash = "iwrupvqb"
 	print "finding answer for key: %s" % hash
-	print find_hash(hash)
+	print find_hash_5(hash)
 
 def parttwo():
-    pass
+	hash = "iwrupvqb"
+	print find_hash_6(hash)
 
 if __name__ == '__main__':
     # test()
-    partone()
+    # partone()
+    parttwo()
