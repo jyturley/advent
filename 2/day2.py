53d5dac81b760b487808c71152ea40f9f78f93b8
jyturley <jyturley@gmail.com>
2015-12-06 13:12:47 -0800

    finish day 2

--- 2/day2.py
+++ 2/day2.py
@@ -14,12 +14,27 @@ def find_dimensions(present):
 
     return surface_area + slack
 
+def find_ribbon_length(present):
+    dimensions = present.split("x")
+    dimensions = [int(d) for d in dimensions]
+    assert(len(dimensions) == 3)
+    l, w, h = dimensions
+
+    dimensions_copy = list(dimensions)
+    dimensions.remove(min(dimensions))
+
+    length_for_present = 2 * min(dimensions) + 2 * min(dimensions_copy)
+    length_for_bow = l * w * h
+
+    return length_for_present + length_for_bow
+
+
 def test():
-    print find_dimensions("2x3x4")
-    print find_dimensions("1x1x10")
+    print find_ribbon_length("2x3x4")
+    print find_ribbon_length("1x1x10")
 
 
-def main():
+def partone():
     total_wrapping_paper = 0
     with open('input.txt', 'r') as f:
         data = f.readlines()
@@ -28,8 +43,15 @@ def main():
 
         print total_wrapping_paper
 
+def parttwo():
+    total_ribbon = 0
+    with open('input.txt', 'r') as f:
+        data = f.readlines()
+        for present in data:
+            total_ribbon += find_ribbon_length(present)
 
+        print total_ribbon
 
 if __name__ == '__main__':
-    # test()
-    main()
+    test()
+    parttwo()
