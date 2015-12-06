6b45f17b20cf59aaf080cf7e4e9881900f4d448f
jyturley <jyturley@gmail.com>
2015-12-06 14:57:59 -0800

    finish day 5

--- 5/day5.py
+++ 5/day5.py
@@ -1,6 +1,26 @@
 #!/bin/python
 
-def is_nice(string):
+def pair_of_letters_constraint(string):
+    for first_letter_index in xrange(len(string) - 1):
+        second_letter_index = first_letter_index + 1
+        pair = string[first_letter_index] + string[second_letter_index]
+        if pair in string[second_letter_index+1:]:
+            return True
+
+    return False
+
+
+def sandwich_letter_constraint(string):
+    for i in xrange(len(string) - 2):
+        if string[i] == string[i + 2]:
+            return True
+
+    return False
+
+def is_nice_2(string):
+    return pair_of_letters_constraint(string) and sandwich_letter_constraint(string)
+
+def is_nice_1(string):
     vowels = set(["a", "e", "i", "o", "u"])
     vowel_count = 0
     double_letter_pass = False
@@ -22,27 +42,41 @@ def is_nice(string):
     return double_letter_pass and vowel_count >= 3
 
 def test():
-    assert(is_nice("ugknbfddgicrmopn") == True)
-    assert(is_nice("aaa") == True)
-    assert(is_nice("jchzalrnumimnmhp") == False)
-    assert(is_nice("haegwjzuvuyypxyu") == False)
-    assert(is_nice("dvszwmarrgswjxmb") == False)
-    print "all tests pass!"
+    assert(is_nice_1("ugknbfddgicrmopn") == True)
+    assert(is_nice_1("aaa") == True)
+    assert(is_nice_1("jchzalrnumimnmhp") == False)
+    assert(is_nice_1("haegwjzuvuyypxyu") == False)
+    assert(is_nice_1("dvszwmarrgswjxmb") == False)
+    print "part one tests pass!"
+
+    assert(pair_of_letters_constraint("aaa") == False)
+    assert(sandwich_letter_constraint("xxyxx") == True)
+    assert(is_nice_2("qjhvhtzxzqqjkmpb") == True)
+    assert(is_nice_2("xxyxx") == True)
+    assert(is_nice_2("uurcxstgmygtbstg") == False)
+    assert(is_nice_2("ieodomkazucvgmuy") == False)
+    print "part two tests pass!"
 
 def partone():
     nice_count = 0
     with open('input.txt', 'r') as f:
         data = f.readlines()
         for string in data:
-            nice_count += is_nice(string)
+            nice_count += is_nice_1(string)
 
     print nice_count
 
 
 def parttwo():
-    pass
+    nice_count = 0
+    with open('input.txt', 'r') as f:
+        data = f.readlines()
+        for string in data:
+            nice_count += is_nice_2(string)
+
+    print nice_count
 
 if __name__ == '__main__':
     test()
-    partone()
-    # parttwo()
+    # partone()
+    parttwo()
