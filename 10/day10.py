89a6de55c285890b7a44f99bae28bb607541a05a
jyturley <jyturley@gmail.com>
2015-12-11 10:52:11 -0800

    day 10 part 1

---
+++ 10/day10.py
@@ -0,0 +1,47 @@
+#!/bin/python
+import re
+
+def look_and_say(input):
+	streak = 1
+	previous = input[0]
+	say = ""
+
+	if len(input) == 1:
+		return "1%s" % input[0]
+
+	for i in xrange(1, len(input)):
+		if input[i] is not previous:
+			say += str(streak) + previous
+			previous = input[i]
+			streak = 1
+		else:
+			streak += 1
+
+	say += str(streak) + previous
+
+	return say
+
+def test():
+	assert(look_and_say("1") == "11")
+	assert(look_and_say("11") == "21")
+	assert(look_and_say("21") == "1211")
+	assert(look_and_say("1211") == "111221")
+	assert(look_and_say("111221") == "312211")
+	print "all tests pass!"
+
+def partone():
+	input = "1113122113"
+	iterations = 40
+
+	for x in xrange(40):
+		input = look_and_say(input)
+
+	print len(input)
+
+def parttwo():
+	pass
+
+if __name__ == '__main__':
+    test()
+    # partone()
+    # parttwo()
