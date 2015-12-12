7f5639ce45f5e67dfee18c1671a541b68d101c24
jyturley <jyturley@gmail.com>
2015-12-12 02:05:07 -0800

    day 11 continuation

--- 11/day11.py
+++ 11/day11.py
@@ -9,7 +9,12 @@ def increment_letter(letter):
 	if letter is'z':
 		return 'a'
 
-	index = modified_ascii_lowercase.index(letter)
+	try:
+		index = modified_ascii_lowercase.index(letter)
+	except:
+		return '$'
+
+	# print modified_ascii_lowercase[index + 1]
 	return modified_ascii_lowercase[index + 1]
 
 
@@ -22,8 +27,51 @@ def increment_string(password):
 
 	return password[:-1] + increment_letter(password[-1])
 
-def valid_pass():
-	pass
+def valid_first_req(password):
+	if len(password) < 3:
+		return False
+
+	streak = 1
+	for i in xrange(len(password) - 1):
+		if streak == 3:
+			return True
+
+		if increment_letter(password[i]) == password[i+1]:
+			streak += 1
+			continue
+
+		streak = 1
+
+	return streak >= 3
+
+def valid_second_req(password):
+	return True
+
+def valid_third_req(password):
+	pairs = []
+
+	i = 0
+	for x in xrange(len(password) - 1):
+		if password[i] == password[i+1]:
+			pairs.append(password[i])
+			i += 1
+			break
+		i += 1
+
+	if not pairs:
+		return False
+
+	for y in xrange(i, len(password) - 1):
+		print y
+		if password[i] == password[i+1] and password[i] != pairs[0]:
+			pairs.append(password[i:i+1])
+		i += 1
+
+	print password, pairs
+	return len(pairs) >= 2
+
+def valid_pass(password):
+	return valid_first_req(password) and valid_second_req(password) and valid_third_req(password)
 
 def find_next_valid_pass(password):
 	max_duration = time.time() + 5 * 60
@@ -39,10 +87,21 @@ def find_next_valid_pass(password):
 def test():
 	assert(increment_letter("a") == "b")
 	assert(increment_letter("z") == "a")
+
 	assert(increment_string("a") == "b")
 	assert(increment_string("z") == "a")
 	assert(increment_string("xz") == "ya")
 	assert(increment_string("zz") == "aa")
+
+	assert(valid_first_req("abc"))
+	# assert(valid_first_req("hijklmn"))
+	# assert(not valid_second_req("hijklmn"))
+
+	assert(not valid_first_req("abbceffg"))
+	assert(valid_third_req("abbceffg"))
+
+	assert(not valid_third_req("abbcegjk"))
+
 	assert(valid_pass("abcdffaa"))
 	assert(valid_pass("ghjaabcc"))
 	assert(find_next_valid_pass("abcdefgh") == "abcdffaa")
@@ -60,5 +119,5 @@ def parttwo():
 
 if __name__ == '__main__':
     test()
-    partone()
+    # partone()
     # parttwo()
