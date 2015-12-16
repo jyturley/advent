cdcc9c1d2914df5ca0f0d3cdaf633b8705992764
jyturley <jyturley@gmail.com>
2015-12-15 17:50:23 -0800

    finish day 12

--- 12/day12.py
+++ 12/day12.py
@@ -2,6 +2,19 @@
 import re
 import json
 
+def find_target(json, target, targets):
+	if isinstance(json, dict):
+		if target not in json.values():
+			for item in json.values():
+				find_target(item, target, targets)
+		else:
+			targets.append(json)
+
+	if isinstance(json, list):
+		for item in json:
+			find_target(item, target, targets)
+
+
 def sum_numbers(input):
 	 return reduce(lambda x,y: x+ y, [int(x) if x else 0 for x in re.sub(r'[^0-9\-]', ' ', input).split()])
 
@@ -26,7 +39,13 @@ def parttwo():
 		data = f.read()
 
 	json_text = json.loads(data)
-	print json.dumps(json_text)
+
+	red_statements = []
+	find_target(json_text, 'red', red_statements)
+	json_red = json.dumps(red_statements)
+
+	print sum_numbers_from_file('input.json') - sum_numbers(json_red)
+
 
 if __name__ == '__main__':
     test()
