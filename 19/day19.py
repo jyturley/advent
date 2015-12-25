afc06a25fa078ec2f55efe7bcb3c5112200c105f
jyturley <jyturley@gmail.com>
2015-12-25 12:35:51 -0800

    start day 19 part 2

--- 19/day19.py
+++ 19/day19.py
@@ -1,7 +1,19 @@
 #!/bin/python
 import re
 
-def parse():
+def generate_replacements(conversions, molecule):
+    replacements = []
+    for key in conversions:
+         for match_index in [m.start() for m in re.finditer(key, molecule)]:
+            for conversion in conversions[key]:
+                # print match_index, key, conversion, len(conversion)
+                # print molecule
+                # print replacement
+                replacement = molecule[:match_index] + conversion + molecule[match_index+len(key):]
+                replacements.append(replacement)
+    return replacements
+
+def parse(reverse=False):
     with open('input.txt', 'r') as f:
         data = f.readlines()
 
@@ -11,7 +23,11 @@ def parse():
     for conversion_or_molecule in data:
         m = re.match(r, conversion_or_molecule)
         if m:
-            key, value = m.groups()
+            if reverse:
+                value, key = m.groups()
+            else:
+                key, value = m.groups()
+
             if not key in conversions:
                 conversions[key] = []
             conversions[key].append(value)
@@ -28,11 +44,14 @@ def partone():
     conversions, molecule = parse()
     print conversions
     print molecule
+    replacements = generate_replacements(conversions, molecule)
+    print len(set(replacements))
 
 def parttwo():
-    pass
+    conversions, molecule = parse(reverse=True)
+    print sorted(conversions, cmp=lambda x,y: 1 if len(x) < len(y) else -1)
 
 if __name__ == '__main__':
     test()
-    partone()
-    # parttwo()
+    # partone()
+    parttwo()
