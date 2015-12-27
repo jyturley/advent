e5ad8eb7473df1f5c9b3eb1bf06597e7bbef2dc8
jyturley <jyturley@gmail.com>
2015-12-26 17:05:10 -0800

    finish day 19

--- 19/day19.py
+++ 19/day19.py
@@ -1,20 +1,41 @@
 #!/bin/python
 import re
+import time
+import sys
+import random
 
-def generate_replacements(conversions, molecule):
+def generate_replacements(conversions, molecule, step_two=False):
     replacements = []
-    for key in conversions:
-         for match_index in [m.start() for m in re.finditer(key, molecule)]:
+    for key in conversions: 
+        for match_index in [m.start() for m in re.finditer(key, molecule)]:
             for conversion in conversions[key]:
-                # print match_index, key, conversion, len(conversion)
-                # print molecule
-                # print replacement
                 replacement = molecule[:match_index] + conversion + molecule[match_index+len(key):]
-                replacements.append(replacement)
+                if not replacement in replacements:
+                    replacements.append(replacement)
     return replacements
 
-def parse(reverse=False):
-    with open('input.txt', 'r') as f:
+def parse_to_tuple(test=False):
+    text_file = 'test_input.txt' if test else 'input.txt'
+    with open(text_file, 'r') as f:
+        data = f.readlines()
+
+    conversions = []
+    r = re.compile(r'(\w+) => (\w+)')
+    for conversion_or_molecule in data:
+        m = re.match(r, conversion_or_molecule)
+        if m:
+            key, value = m.groups()
+            if not key in conversions:
+                conversions[key] = []
+            conversions.append((key, value))
+        else:
+            molecule = conversion_or_molecule.strip()
+
+    return conversions, molecule
+
+def parse(reverse=False, test=False):
+    text_file = 'test_input.txt' if test else 'input.txt'
+    with open(text_file, 'r') as f:
         data = f.readlines()
 
     conversions = {}
@@ -30,14 +51,19 @@ def parse(reverse=False):
 
             if not key in conversions:
                 conversions[key] = []
-            conversions[key].append(value)
+            conversions[key] = value
         else:
-            molecule = conversion_or_molecule
+            molecule = conversion_or_molecule.strip()
 
     return conversions, molecule
 
 def test():
-    molecule = "HOH"
+    conversions, molecule = parse(reverse=True, test=True)
+    # assert(steps_to_target_molecule(conversions, seed_molecule=molecule, target_molecule='e', seen=set()) == 3)
+    # assert(steps_to_target_molecule(conversions, seed_molecule='HOHOHO', target_molecule='e', seen=set()) == 6)
+    print steps_to_target_molecule_recurse(conversions, seed_molecule=molecule, target_molecule='e', seen=set())
+    assert(steps_to_target_molecule_recurse(conversions, seed_molecule=molecule, target_molecule='e', seen=set()) == 3)
+    assert(steps_to_target_molecule_recurse(conversions, seed_molecule='HOHOHO', target_molecule='e', seen=set()) == 6)
     print "all tests pass!"
 
 def partone():
@@ -48,10 +74,28 @@ def partone():
     print len(set(replacements))
 
 def parttwo():
-    conversions, molecule = parse(reverse=True)
-    print sorted(conversions, cmp=lambda x,y: 1 if len(x) < len(y) else -1)
+    conversions, molecule = parse_to_tuple()
+    count = 0
+    mol = molecule
+    shuffle = 0
+    print conversions
+    while len(mol) > 1:
+        start = mol
+        for replacement, to_replace in conversions:
+            while to_replace in mol:
+                count += mol.count(to_replace)
+                mol = mol.replace(to_replace, replacement)
+
+        if start == mol:
+            random.shuffle(conversions)
+            mol = molecule
+            count  = 0
+            shuffle += 1
+            print shuffle
+
+    print count
 
 if __name__ == '__main__':
-    test()
+    # test()
     # partone()
     parttwo()
