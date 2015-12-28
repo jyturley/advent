038e8f498b874306689ee184900ed5b7b585bc39
jyturley <jyturley@gmail.com>
2015-12-28 11:04:02 -0800

    start day 21

---
+++ 21/day21.py
@@ -0,0 +1,20 @@
+#!/bin/python
+import re
+
+def parse():
+    with open('input.txt', 'r') as f:
+        data = f.readlines()
+
+def test():
+    print "all tests pass!"
+
+def partone():
+    pass
+
+def parttwo():
+    pass
+
+if __name__ == '__main__':
+    test()
+    # partone()
+    # parttwo()
