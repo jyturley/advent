12a9afd7ae6a038ea859dd064cef3f379a4163ae
jyturley <jyturley@gmail.com>
2016-01-08 18:14:02 -0800

    day 22: fighters can simulate attack sequences to determine total mana usage

--- 22/day22.py
+++ 22/day22.py
@@ -6,6 +6,7 @@ class Wizard:
         self.damage = damage
 
         self.mana = mana
+        self.total_mana_used = 0
         self.total_mana = mana
         self.total_hp = hp
         self.hp = hp
@@ -27,7 +28,7 @@ class Wizard:
         if self.poison_effect:
             self.poison_effect -= 1
             self.hp -= 3
-            print "Poison timer is now %d" % self.poison_effect
+            print "Poison deals 3 damage. Timer is now %d" % self.poison_effect
             if self.poison_effect <= 0:
                 print "Posion wears off"
 
@@ -43,12 +44,13 @@ class Wizard:
     def recover(self):
         self.hp = self.total_hp
         self.mana = self.total_mana
+        self.total_mana_used = 0
 
     def fight(self, other_player):
         did_you_win = False
         while True:
             print "-- Player turn --"
-            status_check()
+            # status_check()
             if self.take_effect():
                 did_you_win = False
                 break
@@ -62,7 +64,7 @@ class Wizard:
                 break
 
             print "-- Boss turn --"
-            status_check()
+            # status_check()
             if self.take_effect():
                 did_you_win = False
                 break
@@ -81,44 +83,97 @@ class Wizard:
 
     def attack(self, other_player, spell=""):
         damage = self.damage
-        if spell is "Magic Missile":
+        if spell == "Magic Missile":
+            print "Magic Missle is casted for 4 damage"
             self.mana -= 53
+            self.total_mana_used += 53
             damage = 4
 
-        if spell is "Drain":
+        if spell == "Drain":
+            print "Drain is casted for 2 damage"
             self.mana -= 73
+            self.total_mana_used += 73
             damage = 2
             self.hp += 2
 
-        if spell is "Shield":
+        if spell == "Shield":
+            print "Shield is casted"
             self.mana -= 113
+            self.total_mana_used += 113
             self.shield_effect = 6
             self.armor += 7
 
-        if spell is "Poison":
+        if spell == "Poison":
+            print "Poison is casted"
             self.mana -= 173
+            self.total_mana_used += 173
             other_player.poison_effect = 6
 
-        if spell is "Recharge":
+        if spell == "Recharge":
+            print "Recharge is casted"
             self.mana -= 229
+            self.total_mana_used += 229
             self.recharge_effect = 5
 
         other_player.hp -= damage - other_player.armor
         return other_player.hp <= 0
 
+    def atttack_sequence(self, moves, other_player):
+        self.recover()
+        other_player.recover()
+        for move in moves:
+            print "-- Player Turn --"
+            fight_status(self, other_player)
+            if self.take_effect():
+                return -self.total_mana_used
+
+            if other_player.take_effect():
+                return self.total_mana_used
+
+            print "Player casts %s" % move
+            if self.attack(other_player, spell=move):
+                return self.total_mana_used
+
+            print "-- Boss Turn --"
+            fight_status(self, other_player)
+            if self.take_effect():
+                return -self.total_mana_used
+
+            if other_player.take_effect():
+                return self.total_mana_used
+
+            print "Boss attacks for %d damage" % other_player.damage
+            if other_player.attack(self):
+                return -self.total_mana_used
+
+        # raise Exception('Both players are alive. Fight is not over')
+        return -self.total_mana_used
+
+def fight_status(player, boss):
+    print "- %s has %d hp, %d armor, %d mana" % (player.name, player.hp, player.armor, player.mana)
+    print "- Boss has %s hp" % boss.hp
+
+def generate_attack_combos(max_num_moves):
+    pass
+
 def test():
-    you = Wizard(name="player", hp=10, damage=0, mana=250)
-    boss = Wizard(name="boss", hp=13, damage=8, mana=0)
-    assert(not you.attack(boss, spell="Poison"))
-    assert(not boss.attack(you))
-    assert(not you.attack(boss, spell="Magic Missile"))
-    you.recover()
-    boss.recover()
-    assert(you.fight(boss))
+    you = Wizard(name="Player", hp=10, damage=0, mana=250)
+    boss = Wizard(name="Boss", hp=13, damage=8, mana=0)
+    # assert(not you.attack(boss, spell="Poison"))
+    # assert(not boss.attack(you))
+    # assert(not you.attack(boss, spell="Magic Missile"))
+    assert(you.atttack_sequence(["Poison", "Magic Missile"], boss) > 0)
     print "all tests pass!"
 
 def partone():
-    pass
+    you = Wizard(name="Player", hp=10, damage=0, mana=250)
+    boss = Wizard(name="Boss", hp=13, damage=8, mana=0)
+
+    combos = generate_attack_combos(max_num_moves=50)
+    winning_combos_scores = filter(lambda x: x > 0, map(lambda x: you.atttack_sequence(x, boss), combos))
+
+    return min(winning_combos_scores)
+
 
 def parttwo():
     pass
