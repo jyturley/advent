4aa125d30d07f9f8183acf656356512af513b7f9
Jamie Turley <jyturley@gmail.com>
2021-12-08 14:44:36 -0800

    part2

--- 2021/day8/main.go
+++ 2021/day8/main.go
@@ -4,8 +4,29 @@ import (
 	"advent/pkg"
 	"advent/pkg/execute"
 	"fmt"
+	"sort"
 	"strings"
 )
+
+/*
+  0:      1:      2:      3:      4:
+ aaaa    ....    aaaa    aaaa    ....
+b    c  .    c  .    c  .    c  b    c
+b    c  .    c  .    c  .    c  b    c
+ ....    ....    dddd    dddd    dddd
+e    f  .    f  e    .  .    f  .    f
+e    f  .    f  e    .  .    f  .    f
+ gggg    ....    gggg    gggg    ....
+
+  5:      6:      7:      8:      9:
+ aaaa    aaaa    aaaa    aaaa    aaaa
+b    .  b    .  .    c  b    c  b    c
+b    .  b    .  .    c  b    c  b    c
+ dddd    dddd    ....    dddd    dddd
+.    f  e    f  .    f  e    f  .    f
+.    f  e    f  .    f  e    f  .    f
+ gggg    gggg    ....    gggg    gggg
+*/
 
 const (
 	unknown = -8
@@ -29,7 +50,8 @@ func run(input string) (part1 interface{}, part2 interface{}) {
 func Part2(entries []Entry) int {
 	total := 0
 	for _, entry := range entries {
-		total += Analyze(entry)
+		numToPattern := map[int]string{}
+		total += Analyze(entry, numToPattern)
 	}
 	return total
 }
@@ -53,8 +75,148 @@ func Part1(entries []Entry) int {
 	return one + four + seven + eight
 }
 
-func Analyze(entry Entry) int {
+func Analyze(entry Entry, numToPattern map[int]string) int {
+	findOneSevenFourEight(entry, numToPattern)
+	findThree(entry, numToPattern)
+	findFiveTwo(entry, numToPattern)
+	findZeroSixNine(entry, numToPattern)
 
+	return getValue(entry.OutputValues, numToPattern)
+}
+
+func getValue(outputvalues [4]string, numToPattern map[int]string) int {
+	out := [4]int{}
+	for i, ov := range outputvalues {
+		out[i] = match(ov, numToPattern)
+	}
+
+	ans := 1000*out[0] + 100*out[1] + 10*out[2] + out[3]
+	fmt.Println(ans)
+	return ans
+}
+
+func match(ov string, numToPattern map[int]string) int {
+	sov := strings.Split(ov, "")
+	sort.Strings(sov)
+	sortedOV := strings.Join(sov, "")
+	for k, v := range numToPattern {
+		sv := strings.Split(v, "")
+		sort.Strings(sv)
+		sortedV := strings.Join(sv, "")
+		if sortedOV == sortedV {
+			return k
+		}
+	}
+
+	return -10
+}
+
+func findOneSevenFourEight(entry Entry, numToPattern map[int]string) {
+	for _, val := range entry.SignalPatterns {
+		switch len(val) {
+		case 2:
+			numToPattern[1] = val
+		case 3:
+			numToPattern[7] = val
+		case 4:
+			numToPattern[4] = val
+		case 7:
+			numToPattern[8] = val
+		}
+	}
+}
+
+func findZeroSixNine(entry Entry, numToPattern map[int]string) {
+	for _, val := range entry.SignalPatterns {
+		if len(val) == 6 {
+			if contains(val, numToPattern[4]) {
+				numToPattern[9] = val
+			} else {
+				if contains(val, numToPattern[7]) {
+					numToPattern[0] = val
+				} else {
+					numToPattern[6] = val
+				}
+			}
+		}
+	}
+}
+
+func findFiveTwo(entry Entry, numToPattern map[int]string) {
+	for _, val := range entry.SignalPatterns {
+		if len(val) == 5 && val != numToPattern[3] {
+			d := diff(val, numToPattern[4])
+			if len(d) == 2 {
+				numToPattern[5] = val
+			} else {
+				numToPattern[2] = val
+			}
+		}
+	}
+}
+
+func findThree(entry Entry, numToPattern map[int]string) {
+	seven := numToPattern[7]
+	for _, val := range entry.SignalPatterns {
+		if len(val) == 5 && contains(val, seven) {
+			numToPattern[3] = val
+		}
+	}
+}
+
+func diff(bigStr, smallStr string) string {
+	// fmt.Println("diffing ", bigStr, "and", smallStr)
+	out := ""
+	seen := map[string]bool{}
+	for _, sc := range strings.Split(smallStr, "") {
+		seen[sc] = true
+	}
+
+	for _, c := range strings.Split(bigStr, "") {
+		if _, ok := seen[c]; !ok {
+			out += c
+		}
+	}
+
+	return out
+}
+
+func contains(s, threeStr string) bool {
+	seen := map[string]bool{}
+	for _, sc := range strings.Split(s, "") {
+		seen[sc] = true
+	}
+
+	for _, c := range strings.Split(threeStr, "") {
+		if _, ok := seen[c]; !ok {
+			return false
+		}
+	}
+
+	return true
+}
+
+func missingLetter(bigger, smaller string) string {
+	if len(bigger) <= len(smaller) {
+		panic("bad argument")
+	}
+
+	if len(bigger) != len(smaller)+1 {
+		panic("bad argument len")
+	}
+
+	seen := map[string]bool{}
+	for _, sc := range strings.Split(smaller, "") {
+		seen[sc] = true
+	}
+
+	for _, bc := range strings.Split(bigger, "") {
+		if _, ok := seen[bc]; !ok {
+			return bc
+		}
+	}
+
+	return "x"
 }
 
 func parse(s string) []Entry {
--- 2021/day8/main_test.go
+++ 2021/day8/main_test.go
@@ -6,6 +6,41 @@ import (
 )
 
 func TestAnalyze(t *testing.T) {
-	entries := parse(minipizzle)
-	assert.Equal(t, 5353, Analyze(entries[0]))
+	entries := parse(minipuzzle)
+	numToPattern := map[int]string{}
+	assert.Equal(t, 5353, Analyze(entries[0], numToPattern))
+	assert.Equal(t, "cagedb", numToPattern[0], "test for 0")
+	assert.Equal(t, "ab", numToPattern[1], "test for 1")
+	assert.Equal(t, "gcdfa", numToPattern[2], "test for 2")
+	assert.Equal(t, "fbcad", numToPattern[3], "test for 3")
+	assert.Equal(t, "eafb", numToPattern[4], "test for 4")
+	assert.Equal(t, "cdfbe", numToPattern[5], "test for 5")
+	assert.Equal(t, "cdfgeb", numToPattern[6], "test for 6")
+	assert.Equal(t, "dab", numToPattern[7], "test for 7")
+	assert.Equal(t, "cefabd", numToPattern[9], "test for 9")
+	assert.Equal(t, "acedgfb", numToPattern[8], "test for 8")
+
+	entries = parse(testpuzzle)
+	assert.Equalf(t, 8394, Analyze(entries[0], numToPattern), "%v | %v", entries[0].SignalPatterns, entries[0].OutputValues)
+	assert.Equalf(t, 9781, Analyze(entries[1], numToPattern), "%v | %v", entries[1].SignalPatterns, entries[1].OutputValues)
+	assert.Equalf(t, 1197, Analyze(entries[2], numToPattern), "%v | %v", entries[2].SignalPatterns, entries[2].OutputValues)
+	assert.Equalf(t, 9361, Analyze(entries[3], numToPattern), "%v | %v", entries[3].SignalPatterns, entries[3].OutputValues)
+	assert.Equalf(t, 4873, Analyze(entries[4], numToPattern), "%v | %v", entries[4].SignalPatterns, entries[4].OutputValues)
+	assert.Equalf(t, 8418, Analyze(entries[5], numToPattern), "%v | %v", entries[5].SignalPatterns, entries[5].OutputValues)
+	assert.Equalf(t, 4548, Analyze(entries[6], numToPattern), "%v | %v", entries[6].SignalPatterns, entries[6].OutputValues)
+	assert.Equalf(t, 1625, Analyze(entries[7], numToPattern), "%v | %v", entries[7].SignalPatterns, entries[7].OutputValues)
+	assert.Equalf(t, 8717, Analyze(entries[8], numToPattern), "%v | %v", entries[8].SignalPatterns, entries[8].OutputValues)
+	assert.Equalf(t, 4315, Analyze(entries[9], numToPattern), "%v | %v", entries[9].SignalPatterns, entries[9].OutputValues)
+}
+
+func TestContains(t *testing.T) {
+	assert.True(t, contains("abcde", "abc"))
+	assert.True(t, contains("abcde", "bac"))
+	assert.True(t, contains("bcdea", "bac"))
+	assert.False(t, contains("abde", "abc"))
+}
+
+func TestDiff(t *testing.T) {
+	assert.Equal(t, "de", diff("bcdea", "bac"))
+
 }
